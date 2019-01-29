9a5a37d14f4646a95f1e02ef97e138adae5000f0
Jasmine Casey <2325421c@student.gla.ac.uk>
2019-01-29 20:01:02 +0000

    Completed chapter 5 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -28,9 +28,9 @@ def populate():
         {"title":"Flask",
         "url":"http://flask.pocoo.org"} ]
     
-    cats = {"Python": {"pages": python_pages},
-        "Django": {"pages": django_pages},
-        "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+        "Django": {"pages": django_pages, "views": 64, "likes": 32},
+        "Other Frameworks": {"pages": other_pages, "views": 32, "likes" 16} }
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -48,11 +48,14 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes= likes)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
 if __name__ == '__main__':
     print("Starting Rango population script...")
-    populate()
+    populate()
+
